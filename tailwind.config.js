65d5b4c9e6f7a074fef6fce703408c907668609b
Christy David <christy.david.cruze@gmail.com>
2022-11-10 02:22:42 +0000

    New UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.{html,js}","index.html"],
+  content: ["./dist/**/*.{html,js}","./docs/**/*.{html,js}","./*.{html,js}"],
   theme: {
     extend: {},
   },
