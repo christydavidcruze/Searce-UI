f1f7fbde5f995d05036a9cfa989efb16a153e43a
Christy David <christy.david.cruze@gmail.com>
2022-11-11 21:05:13 +0000

    astro build

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
+
+// https://astro.build/config
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [alpinejs(), tailwind()]
+});
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./dist/**/*.{html,js}","./docs/**/*.{html,js}","./*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
